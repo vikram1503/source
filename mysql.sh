2ed1bf3b9ec0c670e336170822dbdc8d2f895839
vikram1503 <vikram15031989@gmail.com>
2024-04-24 16:01:34 +0530

    gone

---
+++ mysql.sh
@@ -0,0 +1,17 @@
+source ./common.sh
+check_root  
+
+echo "please enter db password"
+read -s mysql_root_password
+
+dnf install mysql-server -y &>>$logfile 
+validate $? "installing my sql server"
+
+systemctl enable mysqld &>>$logfile
+validate $? "enabling mysql server"
+
+systemctl start mysqld &>>$logfile
+validate $? "starting mql server"
+ 
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$logfile
+validate $? "setting up root password "
