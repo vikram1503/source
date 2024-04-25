44a67438f1c8659b91b1a6bff0eb4aa700c6a6a5
vikram1503 <vikram15031989@gmail.com>
2024-04-25 13:52:44 +0530

    gone

--- mysql.sh
+++ mysql.sh
@@ -7,21 +7,20 @@ echo "please enter db password:"
 read -s mysql_root_password
 
 dnf install mysqkkl-server -y &>>$logfile 
-#validate $? "installing my sql server"
+
 
 systemctl enable mysqld &>>$logfile
-#validate $? "enabling mysql server"
+
 
 systemctl start mysqld &>>$logfile
-#validate $? "starting mql server"
+
  
-# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$logfile
-# validate $? "setting up root password "
+
 mysql -h db.imvicky.online -uroot -p${mysql_root_password} -e 'show databases;'  &>>$logfile
 if [ $? -ne 0 ]
 then 
-   mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$logfile
-  # validate $? "mysql root password setup"
+   mysql_secure_instalfflation --set-root-pass ${mysql_root_password} &>>$logfile
+  
    else 
    echo -e "mysql root password is already set...$y skipping $n"
    fi
